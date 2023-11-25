813bc6d096b4245d19727c2e3c27e9a3e14cdcce
Carlos L. ✔ <lopes.carlos.host@gmail.com>
2023-11-25 10:53:39 -0300

    feat(API): created all endpoints and receveing get normarlly

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,32 @@
-from django.contrib.auth.models import User # Temporary Model
 from rest_framework import serializers
+from .models import Owner, Animal, AnimalService, Service, Vaccine, Veterinary
 
-class UserSerializer(serializers.ModelSerializer): # Jsonify the User Model
+class OwnerSerializer(serializers.ModelSerializer):
   class Meta:
-    model = User
-    fields = ['id', 'username', 'email']
+    model = Owner
+    fields = ['id', 'name', 'address', 'email']
+
+class AnimalSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Animal
+    fields = ['id', 'name', 'breed', 'age', 'gender', 'owner', 'vaccines', 'services']
+
+class AnimalServiceSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = AnimalService
+    fields = ['id', 'animal', 'service', 'veterinary', 'date']
+
+class ServiceSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Service
+    fields = ['id',  'name', 'schedule_start', 'schedule_end', 'price', 'veterinary']
+
+class VaccineSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Vaccine
+    fields = ['id', 'name', 'date']
+
+class VeterinarySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Veterinary
+    fields = ['id', 'name', 'specialty']
--- api/views.py
+++ api/views.py
@@ -1,7 +1,35 @@
-from django.contrib.auth.models import User # Temporary Model
-from .serializers import UserSerializer
-from rest_framework import viewsets
+from .serializers import OwnerSerializer, AnimalSerializer, AnimalServiceSerializer, ServiceSerializer, VaccineSerializer, VeterinarySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Owner, Animal, AnimalService, Service, Vaccine, Veterinary
 
-class UserViewSet(viewsets.ModelViewSet): # Endpoint for /users
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+class OwnerView(APIView): # endpoint for /api/owner
+   def get(self, request, format=None):
+    serializer = OwnerSerializer(Owner.objects.all(), many=True)
+    return Response(serializer.data)
+
+class AnimalView(APIView): # endpoint for /api/animal
+  def get(self, request, format=None):
+    serializer = AnimalSerializer(Animal.objects.all(), many=True)
+    return Response(serializer.data)
+  
+class AnimalServiceView(APIView): # endpoint for /api/animal-service
+  def get(self, request, format=None):
+    serializer = AnimalServiceSerializer(AnimalService.objects.all(), many=True)
+    return Response(serializer.data)
+  
+class ServiceView(APIView): # endpoint for /api/service
+  def get(self, request, format=None):
+    serializer = ServiceSerializer(Service.objects.all(), many=True)
+    return Response(serializer.data)
+  
+class VaccineView(APIView): # endpoint for /api/vaccine
+  def get(self, request, format=None):
+    serializer = VaccineSerializer(Vaccine.objects.all(), many=True)
+
+    return Response(serializer.data)
+  
+class VeterinaryView(APIView): # endpoint for /api/veterinary
+  def get(self, request, format=None):
+    serializer = VeterinarySerializer(Veterinary.objects.all(), many=True)
+    return Response(serializer.data)
